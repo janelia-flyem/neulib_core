4059f67df70feea796e483036cc83b5b02df6c47
tingzhao <tingzhao@gmail.com>
2020-01-29 15:17:46 -0500

    Adds memoize function

--- include/core/utilities.h
+++ include/core/utilities.h
@@ -1,4 +1,7 @@
 #include <string>
+#include <map>
+#include <mutex>
+#include <tuple>
 
 namespace neulib
 {
@@ -6,12 +9,31 @@ namespace neulib
 template <typename T>
 void Assign(T *out, const T &v)
 {
-  if (out != NULL)
-  {
+  if (out != NULL) {
     *out = v;
   }
 }
 
 std::string ToString(const void *p);
+
+namespace {
+std::mutex memoize_mutex;
+}
+
+template <typename T, typename... Args>
+std::function<T(Args...)> Memoize(T (*fn)(Args...))
+{
+  std::map<std::tuple<Args...>, T> cache;
+  return [fn, cache](Args... args) mutable -> T {
+    std::lock_guard<std::mutex> guard(memoize_mutex);
+    auto argTuple = std::make_tuple(args...);
+    auto memoized = cache.find(argTuple);
+    if (memoized == cache.end()) {
+      return (cache[argTuple] = fn(args...));
+    } else {
+      return memoized->second;
+    }
+  };
+}
 
 } // namespace neutu
